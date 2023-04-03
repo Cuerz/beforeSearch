5138f4bc030821ccb52640a14a08397a11ee807b
Cuerz <sean07260@gmail.com>
2023-04-03 22:30:14 +0800

    commit

---
+++ src/popup/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="popup-header">hello</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.popup-header {
+  width: 100px;
+  height: 100px;
+  color: red;
+}
+</style>
